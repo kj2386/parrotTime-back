2b84b26cdccdcebb080dc23c1193a2af39f6e391
kj2386 <kj2386@gmail.com>
2020-03-12 12:11:46 -0600

    parrot model done

--- parrotTime/models.py
+++ parrotTime/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Parrot(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    gif_url = models.TextField()
+
+    def __str__(self):
+        return self.name
