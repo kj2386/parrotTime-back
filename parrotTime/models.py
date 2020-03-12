afe4de183b6821f205cc366eebc7f3dab5158ea0
kj2386 <kj2386@gmail.com>
2020-03-12 14:08:44 -0600

    cart and order models done

--- parrotTime/models.py
+++ parrotTime/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 class Parrot(models.Model):
     name = models.CharField(max_length=100)
@@ -8,3 +12,23 @@ class Parrot(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    parrot = models.ForeignKey(Parrot, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.quantity} of {self.parrot.name}'
+
+
+class Order(models.Model):
+    order_items = models.ManyToManyField(Cart)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
