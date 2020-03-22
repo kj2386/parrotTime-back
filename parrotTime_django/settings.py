ada3cc72e5e8501f5a60ad34f5a0276e626bc79f
kj2386 <kj2386@gmail.com>
2020-03-22 12:08:55 -0600

    added STATIC_ROOT

--- parrotTime_django/settings.py
+++ parrotTime_django/settings.py
@@ -155,6 +155,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
